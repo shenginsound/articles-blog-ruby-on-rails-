1890cd6f54700b08f470e341479a20a2512942bf
chia-sheng cheng <jaajcy24439@gmail.com>
2024-08-08 17:41:59 -0400

    user_article_association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :user_name, 
         uniqueness: { case_sensitive: false }, 
-        presence: true, length: {minimum :3, maximum:30}
+        presence: true, length: {minimum: 3, maximum: 30}
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i    
     validates :email, presence: true,  
     uniqueness: { case_sensitive: false }, 
-    length: {maximum:30}, format: { with: VALID_EMAIL_REGEX }
+    length: {maximum: 30}, format: { with: VALID_EMAIL_REGEX }
 end
