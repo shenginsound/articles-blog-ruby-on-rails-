0a06db3bd1b6ba08a11300d34847b384b63bad7b
chia-sheng cheng <jaajcy24439@gmail.com>
2024-08-08 16:28:40 -0400

    user vaildation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-
+    validates :user_name, 
+        uniqueness: { case_sensitive: false }, 
+        presence: true, length: {minimum :3, maximum:30}
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i    
+    validates :email, presence: true,  
+    uniqueness: { case_sensitive: false }, 
+    length: {maximum:30}, format: { with: VALID_EMAIL_REGEX }
 end
