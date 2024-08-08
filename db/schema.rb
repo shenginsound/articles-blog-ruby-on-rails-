9712e870efca39b03ff51faaf78de367a55c7fc4
chia-sheng cheng <jaajcy24439@gmail.com>
2024-08-08 15:41:09 -0400

    create db users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_193004) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_08_192009) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
